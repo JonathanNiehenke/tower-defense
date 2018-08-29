c7bf62cf909750107a93d40e7715b74c3bcac658
JonathanNiehenke <jrniehenke@gmail.com>
2018-08-29 07:32:56 -0700

    Move SpriteObj to directDraw.js
    Utils is ambiguous and lazy, which anything can be placed. The name
    directDraw communicates intent of directly drawing to the canvas the
    primary function of SpriteObj.

--- Utils.js
+++ Utils.js
@@ -76,21 +76,6 @@ function PointObj(x, y, type="Cartesian") {
     return this;
 }
 
-function SpriteObj(context, imgSheet, imgRows, imgCols) {
-    this.context = context;
-    this.img = loadImage(imgSheet);
-    this.width = this.img.width / imgCols;
-    this.height = this.img.height / imgRows;
-    this.draw = function(col, row, x, y) {
-        col = (col * this.width) % this.img.width;
-        row = row * this.height;
-        this.context.drawImage(this.img,
-            col, row, this.width, this.height,  // Source
-            x, y, this.width, this.height);  // Destination
-    };
-    return this;
-}
-
 function MenuDisplayObj(sprite, origin, spacing) {
     this.sprite = sprite;
     this.origin = origin;
---
+++ directDraw.js
@@ -0,0 +1,15 @@
+function SpriteObj(context, imgSheet, imgRows, imgCols) {
+    this.context = context;
+    this.img = loadImage(imgSheet);
+    this.width = this.img.width / imgCols;
+    this.height = this.img.height / imgRows;
+    this.draw = function(col, row, x, y) {
+        col = (col * this.width) % this.img.width;
+        row = row * this.height;
+        this.context.drawImage(this.img,
+            col, row, this.width, this.height,  // Source
+            x, y, this.width, this.height);  // Destination
+    };
+    return this;
+}
+
