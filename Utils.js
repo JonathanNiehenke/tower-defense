abfd0f28b708d7040bb6f01ffee21ee1bd2b6ad2
JonathanNiehenke <jrniehenke@gmail.com>
2018-10-23 14:11:15 -0700

    Remove sound

--- Utils.js
+++ Utils.js
@@ -81,18 +81,3 @@ function Point(x, y, type="Cartesian") {
     };
     return this;
 }
-
-function Sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function() {
-        this.sound.play();
-    }
-    this.stop = function() {
-        this.sound.pause();
-    } 
-}
