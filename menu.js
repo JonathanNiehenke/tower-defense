ce8ba642872c31f7b547f833fb34aa69a3e484e1
JonathanNiehenke <jrniehenke@gmail.com>
2018-10-24 07:29:50 -0700

    Reduce the draggable space of tower menu
    To drag a tower from the menu it now requires initiation from above its
    sprite, rather than from the cells far right side.

--- menu.js
+++ menu.js
@@ -1,33 +1,36 @@
 function TowerMenu(sprite, origin, spacing, displayNum) {
     this.menu = new Menu(sprite, origin, spacing);
     this.displayNum = displayNum;
-    this.clicked = undefined;
+    this.dragged = undefined;
     this.draw = function(mousePos) {
         this.drawDrag(mousePos);
         for (let i = 0; i < this.displayNum; ++i)
             this.menu.draw(0, i*3, i, 0);
     };
     this.drawDrag = function(mousePos) {
-        if (this.clicked === undefined) return;
-        const drawPos = mousePos.sub(this.clicked.pos.x, this.clicked.pos.y);
-        sprite.draw(0, this.clicked.value * 3, drawPos.x, drawPos.y);
+        if (this.dragged === undefined) return;
+        const drawPos = mousePos.sub(this.dragged.pos.x, this.dragged.pos.y);
+        sprite.draw(0, this.dragged.value * 3, drawPos.x, drawPos.y);
     };
     this.mouseDown = function(mousePos) {
         const diff = mousePos.sub(origin.x, origin.y);
         const cell = diff.div(
             this.menu.cellWidth, this.menu.cellHeight).floor();
-        if (this.withinMenu(cell)) {
-            this.clicked = {"value": cell.x, "pos": new Point(
-                diff.x % this.menu.cellWidth, diff.y % this.menu.cellHeight)};
-        }
+        const pos = new Point(
+            diff.x % this.menu.cellWidth, diff.y % this.menu.cellHeight);
+        if (this.withinMenu(cell) && this.onSprite(pos))
+            this.dragged = {"value": cell.x, "pos": pos};
     };
     this.withinMenu = function(cell) {
         return cell.y == 0 && cell.x >= 0 && cell.x < this.displayNum;
     };
+    this.onSprite = function(pos) {
+        return pos.x <= sprite.width;
+    };
     this.mouseUpValue = function() {
-        if (this.clicked === undefined) return;
-        const value = this.clicked.value;
-        this.clicked = undefined;
+        if (this.dragged === undefined) return;
+        const value = this.dragged.value;
+        this.dragged = undefined;
         return value;
     };
     return this;
